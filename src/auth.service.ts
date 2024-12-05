ffe957d2a22e72da2b22a72dec0571e9a86ec5be
Michel <michelhenrsilva@gmail.com>
2024-12-05 18:40:10 -0300

    fix

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
     }
 
     const payload = { username: user.username, sub: user.userId };
-    const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    const access_token = this.jwtService.sign(payload)
     return { access_token };
 
     
